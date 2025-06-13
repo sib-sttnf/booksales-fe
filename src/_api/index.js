3d01429da346e1e271e5ec7af1ae902f53c184c7
fakhirula <fakhirula27@gmail.com>
2025-06-14 00:14:00 +0700

    first commit

--- src/_api/index.js
+++ src/_api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const url = "https://akmal-bc.karyakreasi.id";
+const url = "https://pengajarfwd.karyakreasi.id";
 // const url = "http://127.0.0.1:8000";
 
 export const API = axios.create({
